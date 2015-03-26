84f7ee3328a8b1484c43092f8a578084b05a56d6
Tadesse Zemicheal <tadeze@live.com>
2015-03-25 17:36:26 -0700

    base

---
+++ Tree.cpp
@@ -0,0 +1,20 @@
+/*
+ * Tree.cpp
+ *
+ *  Created on: Mar 24, 2015
+ *      Author: Tadeze
+ */
+
+#include "Tree.h"
+using namespace std;
+
+namespace iForest {
+
+
+Tree::~Tree() {
+	// TODO Auto-generated destructor stub
+}
+
+};
+
+/* namespace iForest */
---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 22, 2015
+ *      Author: Tadeze
+ */
+#include<iostream>
+#include<random>
+#include<math.h>
+#include<list>
+#include<vector>
+#define RAND_MAX=10
+using namespace std;
+int main()
+{
+
+
+
+
+//for(int i=0;i<)
+
+
+}
+
+
+
+
